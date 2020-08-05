db335b6c7431f82dc201dcc4d63221228b078cf4
dnmkrgi <xispeech@gmail.com>
2020-08-05 17:08:17 -0400

    Updates

---
+++ precache-manifest.63cb5d2f893682e634838e0a869a2a92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e15f10443da00a05aa4607fa86930e3",
+    "url": "/xi-speech-demo/index.html"
+  },
+  {
+    "revision": "0532e6e1c75a908e34c4",
+    "url": "/xi-speech-demo/static/css/main.df4199d7.chunk.css"
+  },
+  {
+    "revision": "cea5ef954d1866c07604",
+    "url": "/xi-speech-demo/static/js/2.034f3d36.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/xi-speech-demo/static/js/2.034f3d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0532e6e1c75a908e34c4",
+    "url": "/xi-speech-demo/static/js/main.8fee723a.chunk.js"
+  },
+  {
+    "revision": "9aa21d6b9ac2c43218e9",
+    "url": "/xi-speech-demo/static/js/runtime-main.8b3c082b.js"
+  }
+]);
